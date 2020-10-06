9dc699839f104e07e41da4bb9cb9cba3b0622d33
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2020-10-06 15:13:24 +0900

    CAS-13087 port changes from CASA 6 to CASA 5

--- casa5/gcwrap/python/scripts/task_tsdimaging.py
+++ casa5/gcwrap/python/scripts/task_tsdimaging.py
@@ -9,6 +9,7 @@ from taskinit import casalog, gentools, qatool
 
 import sdutil
 import sdbeamutil
+import recipes.mslisthelper as mslisthelper
 
 ## (1) Import the python application layer
 from imagerhelpers.imager_base import PySynthesisImager
@@ -156,9 +157,10 @@ def sort_vis(vislist, spw, mode, width, field, antenna, scan, intent):
     if isinstance(vislist, str) or len(vislist) == 1:
         return vislist, field, spw, antenna, scan, intent
     # chronological sort
-    sorted_vislist = sdutil.tentative_chrono_sort(vislist)
+    sorted_vislist, sorted_timelist = mslisthelper.sort_mslist(vislist)
     _vislist = list(vislist)
     sorted_idx = [_vislist.index(vis) for vis in sorted_vislist]
+    mslisthelper.report_sort_result(sorted_vislist, sorted_timelist, sorted_idx, casalog=casalog)
     fieldsel = SelectionHandler(field)
     sorted_field = [fieldsel(i) for i in sorted_idx]
     spwsel = SelectionHandler(spw)
