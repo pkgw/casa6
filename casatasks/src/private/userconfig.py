3dc462aa47c84e5ecd6570fc8a0568d4cc585df9
Ville Suoranta <vsuorant@nrao.edu>
2022-01-22 07:41:24 -0500

    Revert "Build debug - Revert "fix errors when ~/.casa is missing""
    This reverts commit 61bd7504857d9782b6aceeffe581feead766f27f.

--- casatasks/src/private/userconfig.py
+++ casatasks/src/private/userconfig.py
@@ -1,87 +1,34 @@
-###
-### This is the private module within casatasks which mediates access to
-### user configuration options. It selects which user configuration file
-### to load (only one should be loaded; if desired that file should
-### load secondary files). config.py then pulls supported flags from from
-### this file after validating them.
-###
+"""
+ This is the private module within casatasks which mediates access to
+ user configuration options. It selects which user configuration file
+ to load (only one should be loaded; if desired that file should
+ load secondary files). config.py then pulls supported flags from from
+ this file after validating them.
+"""
 import os
-import sys
-from contextlib import contextmanager
 import pkg_resources
 
-home = os.curdir                        # Default
+home = os.curdir  # Default
 if 'HOME' in os.environ:
     home = os.environ['HOME']
 elif os.name == 'posix':
     home = os.path.expanduser("~/")
-elif os.name == 'nt':                   # Contributed by Jeff Bauer
+elif os.name == 'nt':  # Contributed by Jeff Bauer
     if 'HOMEPATH' in os.environ:
         if 'HOMEDRIVE' in os.environ:
             home = os.environ['HOMEDRIVE'] + os.environ['HOMEPATH']
         else:
             home = os.environ['HOMEPATH']
 
-
-def _fileno(file_or_fd):
-    fd = getattr(file_or_fd, 'fileno', lambda: file_or_fd)()
-    if not isinstance(fd, int):
-        raise ValueError("Expected a file (`.fileno()`) or a file descriptor")
-    return fd
-
-@contextmanager
-def _stdout_redirected(to=os.devnull, stdout=None):
-    if stdout is None:
-       stdout = sys.stdout
-
-    stdout_fd = _fileno(stdout)
-    # copy stdout_fd before it is overwritten
-    #NOTE: `copied` is inheritable on Windows when duplicating a standard stream
-    with os.fdopen(os.dup(stdout_fd), 'wb') as copied: 
-        stdout.flush()  # flush library buffers that dup2 knows nothing about
-        try:
-            os.dup2(_fileno(to), stdout_fd)  # $ exec >&to
-        except ValueError:  # filename
-            with open(to, 'wb') as to_file:
-                os.dup2(to_file.fileno(), stdout_fd)  # $ exec > to
-        try:
-            yield stdout # allow code to be run with the redirected stdout
-        finally:
-            # restore stdout to its previous value
-            #NOTE: dup2 makes stdout_fd inheritable unconditionally
-            stdout.flush()
-            os.dup2(copied.fileno(), stdout_fd)  # $ exec >&copied
-
-@contextmanager
-def _redirect_stdout(new_target):
-    old_target, sys.stdout = sys.stdout, new_target # replace sys.stdout
-    try:
-        yield new_target # run some code with the replaced stdout
-    finally:
-        sys.stdout = old_target # restore to the previous value
-
-def _merged_stderr_stdout():  # $ exec 2>&1
-    return _stdout_redirected(to=sys.stdout, stdout=sys.stderr)
-
-
 # use ~/.casa/config.py if it exists
 # otherwise use default from casaconfig package
 configrc = os.path.join(home, ".casa/config.py")
 configrc = configrc if os.path.exists(configrc) else pkg_resources.resource_filename('casaconfig', 'config.py')
 
-if len(sys.argv) > 0 and sys.argv[0] == '-m':
-    ##  many packages use the idiom "python -m casatools --some-flag" to query for
-    ##  casatools state. Output from user files causes problems for this.
-    with _stdout_redirected(to=os.devnull):
-        try:
-            exec(open(configrc).read())
-            from casataskrc import *
-        except:
-            sys.stderr.write("error: evaluation of %s failed\n" % configrc)
-else:
-    try:
-        exec(open(configrc).read())
-        from casataskrc import *
-    except:
-        sys.stderr.write("error: evaluation of %s failed\n" % configrc)
+# call casashell's casataskrc in case there is a command line argument to overwrite the rcdir
+try:
+    exec(open(configrc).read())
+    from casataskrc import *
+except:
+    pass
 
