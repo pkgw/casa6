90db0e798ad8d1119d2497df09d0a3eaf923ff17
Neal <nschweig@nrao.edu>
2022-03-29 12:10:42 -0400

    Added a new row to the observation table in the test_selectVisObs. Missing this would cause a seg-fault

--- casatools/tests/tools/calibrater/test_tool_calibrater.py
+++ casatools/tests/tools/calibrater/test_tool_calibrater.py
@@ -416,13 +416,17 @@ class calibrater_test(unittest.TestCase):
 
         self.assertFalse(np.array_equal(datacol, beforedata))
 
-    @unittest.skip("SKIPPING test_selectVisObs UNTIL TEST IS REPAIRED")
+
     def test_selectVisObs(self):
         # Edit table to have multiple obs IDs
         tb.open(self._vis, nomodify=False)
         obsids = tb.getcol('OBSERVATION_ID')
         obsids[10:10000] = 1
         tb.putcol('OBSERVATION_ID', obsids)
+        tb.close()
+        # Add a new row to the observation subtable
+        tb.open(self._vis+'/OBSERVATION', nomodify=False)
+        tb.copyrows(self._vis+'/OBSERVATION')
         tb.close()
 
         tb.open(self._cal, nomodify=False)
@@ -431,17 +435,7 @@ class calibrater_test(unittest.TestCase):
         tb.putcol('OBSERVATION_ID', obsids)
         tb.close()
 
-        rowswithobs = []
-        rowswithoutobs = []
         tb.open(self._vis)
-        datacol = tb.getcol('OBSERVATION_ID')
-
-        for i in range(len(datacol)):
-            if datacol[i] == 0:
-                rowswithobs.append(i)
-            else:
-                rowswithoutobs.append(i)
-
         # Now save the DATA column to compare later
         beforedata = tb.getcol('DATA')[0][0]
         tb.close()
