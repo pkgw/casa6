b9ee70fc9b0015b78652bdf8e893db588efec325
Benjamin Bean <bbean@nrao.edu>
2022-06-03 17:15:15 -0600

    CAS-13814 update tclean for permanent casa6 transition

--- casatasks/src/private/task_tclean.py
+++ casatasks/src/private/task_tclean.py
@@ -13,37 +13,19 @@ import numpy
 import copy
 import filecmp
 import time
-# get is_CASA6 and is_python3
-from casatasks.private.casa_transition import *
-if is_CASA6:
-    from casatasks import casalog
+from casatasks import casalog
+from casatasks.private.imagerhelpers.imager_base import PySynthesisImager
+from casatasks.private.imagerhelpers.input_parameters import saveparams2last
+from casatasks.private.imagerhelpers.imager_parallel_continuum import PyParallelContSynthesisImager
+from casatasks.private.imagerhelpers.imager_parallel_cube import PyParallelCubeSynthesisImager
+from casatasks.private.imagerhelpers.input_parameters import ImagerParameters
+from .cleanhelper import write_tclean_history, get_func_params
+from casatools import table
+from casatools import synthesisimager
 
-    from casatasks.private.imagerhelpers.imager_base import PySynthesisImager
-    from casatasks.private.imagerhelpers.input_parameters import saveparams2last
-    from casatasks.private.imagerhelpers.imager_parallel_continuum import PyParallelContSynthesisImager
-    from casatasks.private.imagerhelpers.imager_parallel_cube import PyParallelCubeSynthesisImager
-    from casatasks.private.imagerhelpers.input_parameters import ImagerParameters
-    from .cleanhelper import write_tclean_history, get_func_params
-    from casatools import table
-    from casatools import synthesisimager
-else:
-    from taskinit import *
-
-    from imagerhelpers.imager_base import PySynthesisImager
-    from imagerhelpers.imager_parallel_continuum import PyParallelContSynthesisImager
-    from imagerhelpers.imager_parallel_cube import PyParallelCubeSynthesisImager
-    from imagerhelpers.input_parameters import ImagerParameters
-    from imagerhelpers.input_parameters import saveparams2last
-    from cleanhelper import write_tclean_history, get_func_params
-    table=casac.table
-    synthesisimager=casac.synthesisimager
 try:
-    if is_CASA6:
-        from casampi.MPIEnvironment import MPIEnvironment
-        from casampi import MPIInterface
-    else:
-        from mpi4casa.MPIEnvironment import MPIEnvironment
-        from mpi4casa import MPIInterface
+    from casampi.MPIEnvironment import MPIEnvironment
+    from casampi import MPIInterface
     mpi_available = True
 except ImportError:
     mpi_available = False
@@ -248,10 +230,7 @@ def tclean(
     # Put all parameters into dictionaries and check them.
     ##make a dictionary of parameters that ImagerParameters take
 
-    if is_python3:
-        defparm=dict(list(zip(ImagerParameters.__init__.__code__.co_varnames[1:], ImagerParameters.__init__.__defaults__)))
-    else:
-        defparm=dict(zip(ImagerParameters.__init__.__func__.__code__.co_varnames[1:], ImagerParameters.__init__.func_defaults))
+    defparm=dict(list(zip(ImagerParameters.__init__.__code__.co_varnames[1:], ImagerParameters.__init__.__defaults__)))
         
     ###assign values to the ones passed to tclean and if not defined yet in tclean...
     ###assign them the default value of the constructor
@@ -294,12 +273,8 @@ def tclean(
     if mpi_available and MPIEnvironment.is_mpi_enabled and specmode!='mfs' and not pcube:
         mint=MPIInterface.MPIInterface()
         cl=mint.getCluster()
-        if(is_CASA6):
-            cl._cluster.pgc("from casatools import synthesisimager", False)
-            cl._cluster.pgc("si=synthesisimager()", False)
-        else:
-            cl._cluster.pgc("from casac import casac", False)
-            cl._cluster.pgc("si=casac.synthesisimager()", False) 
+        cl._cluster.pgc("from casatools import synthesisimager", False)
+        cl._cluster.pgc("si=synthesisimager()", False)
         cl._cluster.pgc("si.initmpi()", False)
         cppparallel=True
         ###ignore chanchunk
