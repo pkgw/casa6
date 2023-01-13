21a9276d2a05cb755fc9eb7d2e6b46891d0aa28f
Ville Suoranta <vsuorant@nrao.edu>
2023-01-13 12:16:02 -0500

    Add else block to the MPI check in Applicator.cc

--- casatools/src/code/synthesis/Parallel/Applicator.cc
+++ casatools/src/code/synthesis/Parallel/Applicator.cc
@@ -81,28 +81,31 @@ void Applicator::initThreads(Int argc, Char *argv[]){
 #ifdef HAVE_MPI
   //if (debug_p) {
   if(initialized_p) return;
-  ///If detecting only  1 proc is offered to OpenMPI but compiling with MPI
+  
+  //If detecting only  1 proc is offered to OpenMPI but compiling with MPI
   if (!getenv("OMPI_COMM_WORLD_LOCAL_SIZE") ||  (String::toInt(getenv("OMPI_COMM_WORLD_LOCAL_SIZE")) <2) ) {
     //go serial
     initThreads();
-  }
-  //  cerr << "In initThreads. argc: " << argc << ", argv: " << argv << '\n';
+  } 
+  else {
+    //cerr << "In initThreads. argc: " << argc << ", argv: " << argv << '\n';
       //}
-  // Initialize the MPI transport layer
-  try {
-     comm = new MPITransport(argc, argv);
+    // Initialize the MPI transport layer
+    try {
+      comm = new MPITransport(argc, argv);
 
-     // Initialize the process status list
-     setupProcStatus();
+       // Initialize the process status list
+       setupProcStatus();
 
-     // If controller then exit, else loop, waiting for an assigned task
-     if (isWorker()) {
-       loop();
-     }
+       // If controller then exit, else loop, waiting for an assigned task
+       if (isWorker()) {
+         loop();
+       }
 
-  } catch (MPIError x) {
-    cerr << x.getMesg() << " doing serial "<< endl;
-    initThreads();
+    } catch (MPIError x) {
+      cerr << x.getMesg() << " doing serial "<< endl;
+      initThreads();
+    }
   } 
 
 #else
