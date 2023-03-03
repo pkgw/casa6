bed8ecaec1425afb716591a587f1e49bb63da9d7
Takeshi Nakazato <takeshi.nakazato@nao.ac.jp>
2023-03-03 11:24:54 +0900

    CAS-14072 adapt casacore changes
    updated classes affected by the replacement of CountedPtr with std::shared_ptr

--- casatools/src/code/synthesis/Parallel/MPITransport.cc
+++ casatools/src/code/synthesis/Parallel/MPITransport.cc
@@ -27,6 +27,8 @@
 
 #ifdef HAVE_MPI
 
+#include <memory>
+
 #include <casacore/casa/Containers/Record.h>
 #include <casacore/casa/IO/AipsIO.h>
 #include <casacore/casa/IO/MemoryIO.h>
@@ -83,7 +85,7 @@ Bool MPITransport::isFinalized()
     return Bool(flag);
 }
 
-Int MPITransport::anyTag() 
+Int MPITransport::anyTag()
 {
 // Return the value which indicates an unset tag
 //
@@ -233,8 +235,8 @@ Int MPITransport::put(const Bool &b){
 
 Int MPITransport::put(const Record &r){
    setDestAndTag(sendTo, myOp);
-   MemoryIO buffer;
-   AipsIO rBuf(&buffer);
+   std::shared_ptr<MemoryIO> buffer = std::make_shared<MemoryIO>();
+   AipsIO rBuf(buffer);
    rBuf.putstart("MPIRecord",1);
    rBuf << r;
    rBuf.putend();
@@ -242,7 +244,7 @@ Int MPITransport::put(const Record &r){
     //cerr << "Bytes 2 send " << bytes2send << endl;
    // warning: sstat set but not used!
    Int sstat = MPI_Send((void *)&bytes2send, 1, MPI_UNSIGNED, sendTo, myOp, MPI_COMM_WORLD);
-   sstat = MPI_Send((void *)buffer.getBuffer(), bytes2send, MPI_UNSIGNED_CHAR, sendTo, myOp,
+   sstat = MPI_Send((void *)buffer->getBuffer(), bytes2send, MPI_UNSIGNED_CHAR, sendTo, myOp,
                     MPI_COMM_WORLD);
    (void) sstat; // warning: unused sstat
    return(0);
@@ -465,8 +467,8 @@ Int MPITransport::get(Record &r){
    // Now fill the buffer full of bytes from the record
    std::vector<uChar> buffer(bytesSent);
    MPI_Recv(buffer.data(), bytesSent, MPI_UNSIGNED_CHAR, getFrom, myOp, MPI_COMM_WORLD, &status);
-   MemoryIO nBuf(buffer.data(), bytesSent);
-   AipsIO rBuf(&nBuf);
+   std::shared_ptr<MemoryIO> nBuf = std::make_shared<MemoryIO>(buffer.data(), bytesSent);
+   AipsIO rBuf(nBuf);
    uInt version = rBuf.getstart("MPIRecord");
    (void)version; // warning: unused version
    rBuf >> r;
