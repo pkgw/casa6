ea37f5e3368b0bf3da65269582f4b1f6c2663417
Renaud Miel <renaud.miel@nao.ac.jp>
2023-03-22 17:59:41 +0900

    CAS-14025 SynthesisImagerVi2::createFTMachine
    Header file. Propagate convertfirst parameter

--- casatools/src/code/synthesis/ImagerObjects/SynthesisImagerVi2.h
+++ casatools/src/code/synthesis/ImagerObjects/SynthesisImagerVi2.h
@@ -160,6 +160,7 @@ public:
 		       const casacore::String stokes="I",
 		       const casacore::String imageNamePrefix="",
 		       const casacore::String &pointingDirCol=casacore::String("direction"),
+           const casacore::String &convertFirst=casacore::String("direction"),
 		       const casacore::Float skyPosThreshold=0.0,
 		       const casacore::Int convSupport=-1,
 		       const casacore::Quantity &truncateSize=casacore::Quantity(-1),
