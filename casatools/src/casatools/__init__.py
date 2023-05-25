451123d509b90e44e285d60f54bb498d3ba762e1
Bob Garwood <bgarwood@nrao.edu>
2023-05-25 06:56:24 -0400

    resolve differences between __init__.py and genertaed __init__.py in setup.py.in

--- casatools/src/casatools/__init__.py
+++ casatools/src/casatools/__init__.py
@@ -78,101 +78,43 @@ from .quanta import quanta
 from .synthesisimager import synthesisimager
 from .componentlist import componentlist
 from .spectralline import spectralline
-from casatools import ctuser as __user
 from .utils import utils as __utils
 import os as __os
 import sys as __sys
-
-def __find_data_path( ):
-
-    def find_mount_point(path):
-        path = __os.path.abspath(path)
-        while not __os.path.ismount(path):
-            path = __os.path.dirname(path)
-        return path
-
-    #potential_data_paths = ['/opt/casa/data', '/home/casa/data/master', '/home/casa/data', '/export/data_1/casa/data']
-    potential_data_paths = [ ]
-    casadata = [ d for d in (__os.environ['CASADATA'].split(':') if 'CASADATA' in __os.environ else [ ]) if __os.path.isdir(d) ]
-    potential = [ d for d in potential_data_paths if __os.path.isdir(d) ]
-    potential_local = [ d for d in potential if find_mount_point(d) == '/' ]
-    potential_remote = [ d for d in potential if find_mount_point(d) != '/' ]
-    used = set( )
-    return [ x for x in casadata + potential_local + potential_remote if x not in used and (used.add(x) or True)]
-
-def __find_user_data_path( ):
-    def is_iter_container(v):
-        try:
-            _iter = iter(v)
-            return not isinstance(v,str)
-        except TypeError:
-            return False
-
-    if hasattr(__user,'datapath') and is_iter_container(__user.datapath):
-        return list(filter(__os.path.isdir, list(map(__os.path.expanduser,__user.datapath))))
-    else:
-        return [ ]
-
-def __find_user_nogui( ):
-  result = False
-  if (hasattr(__user,'nogui') and isinstance(__user.nogui,bool)):
-     result = __user.nogui
-  return result
-
-def __find_user_agg( ):
-  result = False
-  if (hasattr(__user,'agg') and isinstance(__user.agg,bool)):
-     result = __user.agg
-  return result
-
-def __find_user_pipeline( ):
-  result = False
-  if (hasattr(__user,'pipeline') and isinstance(__user.pipeline,bool)):
-     result = __user.pipeline
-  return result
+from casaconfig import pull_data, measures_update, config
 
 sakura( ).initialize_sakura( )    ## sakura requires explicit initialization
 
+
+user_datapath = config.datapath
+user_nogui = config.nogui
+user_agg = config.agg
+user_pipeline = config.pipeline
+user_cachedir = __os.path.abspath(__os.path.expanduser(config.cachedir))
+
+user_measurespath = config.measurespath
+if (__sys.argv[0] != '-m') and (not __os.path.isdir(__os.path.realpath(user_measurespath))):
+    print('measurespath path not found, creating %s' % user_measurespath)
+    __os.makedirs(user_measurespath)
+
 ctsys = __utils( )
-_distro_dpath = None
-_dpath = [ ]
+ctsys.initialize( __sys.executable, user_measurespath, user_datapath, user_nogui,
+                  user_agg, user_pipeline, user_cachedir )
 
-_user_data = None
-if hasattr(__user,'rundata'):
-     if not __os.path.isdir(__os.path.expanduser(__user.rundata)):
-         if __sys.argv[0] != '-m':
-             print("ignoring rundata setting (%s) because it is not a directory" % __user.rundata,file=__sys.stderr)
-     else:
-         _user_data = __os.path.expanduser(__user.rundata)
+logger = logsink(config.logfile) if (hasattr(config,'logfile') and config.logfile is not None) else None
 
-if _user_data is None:
-    try:
-        import casashell as __cs
-        _user_data = __os.path.expanduser(__os.path.join(__cs._rcdir,'data'))
-    except Exception as e:
-        _user_data = __os.path.expanduser("~/.casa/data")
+if (__sys.argv[0] != '-m') and (hasattr(config,'configrc')):
+    print('Using %s' % config.configrc)
+    if logger is not None:
+        logger.post('Using %s' % config.configrc, 'INFO')
 
-if __os.path.isdir(_user_data):
-    _dpath = _dpath + [ _user_data ]
-    _iers = __os.path.join(_user_data,'geodetic','IERSeop2000')
-    if __os.path.isdir(_iers):
-        _distro_dpath = _user_data
-
-if _distro_dpath is None:
-    try:
-        import casadata
-        _distro_dpath = casadata.datapath
-        _dpath = _dpath + [ _distro_dpath ]
-    except: pass
-
-ctsys.initialize( __sys.executable, "" if _distro_dpath is None else _distro_dpath,
-                  __find_user_data_path( ) + _dpath,
-                  __find_user_nogui( ), __find_user_agg( ), __find_user_pipeline( ) )
+if hasattr(config,'measures_update') and (config.measures_update == True) and (__sys.argv[0] != '-m'):
+    measures_update(ctsys.rundata(), logger=logger)
 
 if __sys.argv[0] != '-m':
     __resolved_iers = ctsys.resolve('geodetic/IERSeop2000')
     if __resolved_iers == 'geodetic/IERSeop2000':
-        raise ImportError('measures data is not available, visit http://go.nrao.edu/casadata-info for more information')
+        raise ImportError('measures data is not available, visit https://casadocs.readthedocs.io/en/stable/notebooks/external-data.html for more information')
     if len(ctsys.rundata( )) == 0:
         ctsys.setrundata(__resolved_iers[:-21])
 
