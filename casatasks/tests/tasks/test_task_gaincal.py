29fe274364d6943a98687912bc9f2e0073aaa2f5
Neal Schweighart <nschweig@nrao.edu>
2023-05-26 15:30:45 -0400

    Fixed issue with gencal missing from imports

--- casatasks/tests/tasks/test_task_gaincal.py
+++ casatasks/tests/tasks/test_task_gaincal.py
@@ -29,7 +29,7 @@ import numpy as np
 import pylab as pl
 
 import casatools
-from casatasks import gaincal, mstransform, casalog, flagdata
+from casatasks import gaincal, mstransform, casalog, flagdata, gencal
 tb = casatools.table()
 from casatestutils import testhelper as th
 
