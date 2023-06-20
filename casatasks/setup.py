d5a15e88866806f63c7facab4fb9a6ff8c98dd48
Darrell Schiebel <darrell@schiebel.us>
2023-06-20 11:49:33 -0400

    fix kwarg + task banner bug [CAS-14106]

--- casatasks/setup.py
+++ casatasks/setup.py
@@ -476,7 +476,7 @@ class BuildCasa(build):
             proc = Popen( [tools_config['build.compiler.xml-casa'], "output-task=%s" % moduledir, "-task"] + xml_files,
                           stdout=subprocess.PIPE )
         else:
-            xml_jar_file = 'xml-casa-assembly-1.79.jar'
+            xml_jar_file = 'xml-casa-assembly-1.81.jar'
             xml_jar_url = 'http://casa.nrao.edu/download/devel/xml-casa/java/%s' % xml_jar_file
             xml_jar_path = os.path.abspath(os.path.join( 'java', xml_jar_file))
             self.xml_jar_fetch(xml_jar_path, xml_jar_url)
