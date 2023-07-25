e91c80685c8490e38d1904b41cb5b87755642225
Dave Mehringer <dmehring@nrao.edu>
2023-07-25 12:55:25 -0400

    initial commit of calmod skeleton

---
+++ casatasks/src/private/task_calmod.py
@@ -0,0 +1,11 @@
+def calmod(
+    outfile, source, direction, band, obsdate, refdate, hosts
+):
+    print(f'outfile is {outfile}')
+    print(f'source is {source}')
+    print(f'direction is {direction}')
+    print(f'band is {band}')
+    print(f'obsdate is {obsdate}')
+    print(f'refdate is {refdate}')
+    print(f'hosts is {hosts}')
+
