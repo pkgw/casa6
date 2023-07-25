ceb428a60e9e5c795e316e5a9b0acd83849b9f3d
Dave Mehringer <dmehring@nrao.edu>
2023-07-25 19:44:57 -0400

    some exception code

--- casatasks/src/private/task_calmod.py
+++ casatasks/src/private/task_calmod.py
@@ -1,11 +1,16 @@
 def calmod(
     outfile, source, direction, band, obsdate, refdate, hosts
 ):
-    print(f'outfile is {outfile}')
-    print(f'source is {source}')
-    print(f'direction is {direction}')
-    print(f'band is {band}')
+    print(f'outfile is "{outfile}"')
+    print(f'source is "{source}"')
+    print(f'direction is "{direction}"')
+    print(f'band is "{band}"')
     print(f'obsdate is {obsdate}')
     print(f'refdate is {refdate}')
     print(f'hosts is {hosts}')
-
+    if not outfile.strip():
+        raise RuntimeError('outfile must be specified')
+    if not (source.strip() or direction.strip()):
+        raise RuntimeError('Exactly one of source or direction must be specified')
+    if source and direction:
+        raise RuntimeError('Both source and direction may not be simultaneously specified')
