d662e4b688e63a4a8fbb4936d8c174c6628c7909
Srikrishna Sekhar <ssekhar@nrao.edu>
2023-08-23 17:29:29 -0600

    CAS-13902 : Fix bug with multi-MS multi-SPW config
    Previously when two or more MSs were passed in to tclean, and the MSs
    had different SPW configurations tclean would segfault and crash.
    
    This has now been fixed. Interally, the unique SPWs per MS are being
    kept track of and imaging seems to work as expected.

--- casatools/src/code/synthesis/ImagerObjects/SynthesisUtilMethods.cc
+++ casatools/src/code/synthesis/ImagerObjects/SynthesisUtilMethods.cc
@@ -2396,6 +2396,21 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 	  if(imStartFreq > 0.0 && imStartFreq >= freqmin && imStartFreq <= freqmax){
             if(mode != "cubesource"){
               minfmsid=j;
+
+              // CAS-13902 - If multiple MSs are passed in, we need to only keep the
+              // new SPWs for every additional MS.
+              // Figure out which SPWs need to be pushed in and only push those in.
+              for(auto ii = 0; ii < spwids.size(); ii++) {
+                if (ii >= spwids0.size()) { // If spwids0 is too small
+                  auto n = spwids0.size();
+                  spwids0.resize(n+1, True);
+                }
+                // SPW not found in spwids0, so add it in
+                if (std::find(spwids0.begin(), spwids0.end(), spwids(ii)) == spwids0.end()) {
+                    spwids0[ii] = spwids(ii);
+                }
+              }
+
               spwids0=spwids;
               vi2.originChunks();
               vi2.origin();
