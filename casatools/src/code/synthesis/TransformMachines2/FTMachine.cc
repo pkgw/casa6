f23c4a6a9ab1b236b304916a38bfc8d13f6af7be
Renaud Miel <renaud.miel@nao.ac.jp>
2024-01-12 11:20:03 +0900

    CAS-14025 FTMachine.cc: code clean-up

--- casatools/src/code/synthesis/TransformMachines2/FTMachine.cc
+++ casatools/src/code/synthesis/TransformMachines2/FTMachine.cc
@@ -2497,13 +2497,6 @@ using namespace casa::vi;
   
   // Vectorized finalizeToVis is not implemented because it does nothing and is never called.
 
-  // Handle new MeasurementSet
-  // void handleNewMs(const casacore::MeasurementSet & /* ms */,
-  //              casacore::CountedPtr<SIImageStore> /* imstore */)
-  // {
-  //   // By default do nothing
-  // }
-
   // Vectorized InitializeToSky
   void FTMachine::initializeToSkyNew(const Bool dopsf, 
 				     const VisBuffer2& vb,
