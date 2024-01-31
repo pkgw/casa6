bd127861ff582ab1718fd5f3e39a45aea8feb389
Renaud Miel <renaud.miel@nao.ac.jp>
2024-01-31 16:07:31 +0900

    CAS-14025 Removed noisy log messages.
    Log file removal message only when file exists

--- casatasks/src/private/task_tsdimaging.py
+++ casatasks/src/private/task_tsdimaging.py
@@ -959,13 +959,15 @@ def tsdimaging(
                       )
             else:
                 # delete existing images
-                casalog.post(f"Removing '{singledish_image_path}'")
-                _remove_image(singledish_image_path)
+                if os.path.exists(singledish_image_path):
+                    casalog.post(f"Removing '{singledish_image_path}'")
+                    _remove_image(singledish_image_path)
                 assert not os.path.exists(singledish_image_path)
                 for _suffix in associate_suffixes:
                     path_to_remove = output_path_prefix + _suffix
-                    casalog.post(f"Removing '{path_to_remove}'")
-                    _remove_image(path_to_remove)
+                    if os.path.exists(path_to_remove):
+                        casalog.post(f"Removing '{path_to_remove}'")
+                        _remove_image(path_to_remove)
                     assert not os.path.exists(path_to_remove)
 
         # Tweak spw parameter into _spw
