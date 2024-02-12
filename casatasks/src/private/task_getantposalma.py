8bee9ba3c37c5edf63a676bd12c987b5567874de
Dave Mehringer <dmehring@nrao.edu>
2024-02-12 16:13:55 -0500

    new doc updates

--- casatasks/src/private/task_getantposalma.py
+++ casatasks/src/private/task_getantposalma.py
@@ -46,11 +46,12 @@ Retrieve antenna positions by querying ALMA web service.
 
 Parameters
    - outfile_ (path='') - Name of output file to which to write retrieved antenna positions.
+   - overwrite_ (bool=False) - Overwrite a file by the same name if it exists?
    - asdm_ (string='') - The associated ASDM name. Must be specified
-   - tw_ (string='') - Optional time window to which to limit search for antenna positions.
+   - tw_ (string='') - Optional time window in which to consider baseline measurements in the database, when calculating the antenna positions.
    - snr_ (float=0) - Optional signal-to-noise.
    - search_ (string='both_latest') - Search algorithm to use.
-   - hosts_ (stringVec=['tbd1.alma.cl', 'tbd2.alma.cl']) - Priority-ranked list of hosts to query.
+   - hosts_ (stringVec=['https://asa.alma.cl/uncertainties-service/uncertainties/versions/last/measurements/casa/']) - Priority-ranked list of hosts to query.
 
 
 
@@ -58,20 +59,33 @@ Parameters
 .. _Description:
 
 Description
-Antpos retrieves ALMA antenna positions via a web service which runs on an
-ALMA-hosted server. The antenna positions are with respect to ITRF. The
-user must specify the value of the outfile parameter. This parameter is
-the name of the file to which the antenna positions will be written. This
-file can then be read by gencal so that it can use the most up to date
-antenna positions for the observation.
+
+.. warning:: **WARNING**: This task should be considered experimental
+   since the values returned by the JAO service are in the process of
+   being validated.
+
+This task retrieves ALMA antenna positions via a web service which runs
+on an ALMA-hosted server. The antenna positions are with respect to ITRF.
+The user must specify the value of the outfile parameter. This parameter
+is the name of the file to which the antenna positions will be written.
+This file can then be read by gencal so that it can use the most up to
+date antenna positions for the observation.
+
+The web service is described by the server development team and can be
+found `at this location <https://asw.alma.cl/groups/ASW/-/packages/843>`__. 
 
 The input parameters are discussed in detail below.
 
 outfile is required to be specified. It is the name of the file to which to
-write antenna positions. If a file with the same name exists it will be
-silently overwritten.
+write antenna positions.
 
-asdm is required to be specified. It is the associated ASDM name.
+overwrite If False and a file with the same name exists, and exception
+will be thrown. If true, an existing file with the same name will be
+overwriten.
+
+asdm is required to be specified. It is the associated ASDM UID in the
+form uid://A002/Xc02418/X29c8. 
+
 tw is an optional parameter. It is time window in which the antenna positions
 are required, specified as a comma separated pair. Times are UTC and are
 expressed in YY-MM-DDThh:mm:ss.sss format. The end time must be later than
@@ -81,21 +95,43 @@ snr is an optional parameter. It is the signal-to-noise ratio. Antenna
 positions which have corrections less than this value will not be written.
 If not specified, positions of all antennas will be written.
 
-search is an optional parameter, It is the search algorithm to use.
-Supported values are 'both_latest' and 'both_closest'. For 'both_latest',
-the last updated position for each antenna within 30 days after the
-observation will be returned, taking into account snr if specified. If provided,
+tw and search are optional parameters and are coupled as follows. search
+indicates the search algorithm to use to find the desired antenna positions.
+Supported values of this parameter at the time of writing are 'both_latest'
+and 'both_closest'. The task passes the value of the search parameter verbatim to
+the web service, meaning that users can take advantage of new search algorithms
+as the web service team brings them online. The default algorithm used is
+'both_latest'. In general, the search is limited in time to the specified
+value of tw (time window). However, in the case that tw is not specified, the
+following rules apply. For 'both_latest', the last updated position for each
+antenna within the specified time window, or, if tw is not specified, within
+30 days after the observation will be returned, taking into account snr if
+specified, if provided.
 
-tw will override the 30 day default value. For 'both_closest', the position
+For 'both_closest', if tw is not specified, the position
 of each antenna closest in time to the observation, within 30 days (before
-or after the observation will be returned, subject to the value of snr if it
-is specified. If specified, the value of tw will override the default 30 days.
-The default algorithm used is 'both_latest'.
+or after the observation) will be returned, subject to the value of snr if it
+is specified. 
 
-hostss is a required parameter. It is a list of hosts to query, in order of
+hosts is a required parameter. It is a list of hosts to query, in order of
 priority, to obtain positions. The first server to respond with a valid result is
 the only one that is used. That response will be written and no additional
-hostss will be queried.
+hosts will be queried.
+
+The format of the returned file is a two element list encoded in json. The first
+element is a stringfied dictionary that contains antenna names as keys, with each
+value being a three element list of x, y, and z coordinates in ITRF. The second
+element is a dictionary containing various (possibly helpful) metadata that were
+used when the task was run. The following code may be used to load these data
+structures into python variables.
+    
+    ::
+        
+        import ast, json
+        ...
+        with open("outfile.json", "r") as f:
+            antpos_str, md_dict = json.load(f)
+            antpos_dict = ast.literal_eval(antpos_str)
 
 
 .. _Examples:
@@ -106,7 +142,7 @@ Examples
    
    ::
    
-      antpos(
+      getantposalma(
           outfile='my_ant_pos.json', asdm='valid ASDM name here', snr=5,
           hosts=['tbd1.alma.cl', 'tbd2.alma.cl']
      )
@@ -132,15 +168,20 @@ Parameter Details
 |    default: none
 |    Example: outfile='my_alma_antenna_positions.json'
 
+.. _overwrite:
+
+| ``overwrite (bool=False)`` - Overwrite a file by the same name if it exists? If False and a file
+|                with the same name exists, and exception will be thrown.
+
 .. _asdm:
 
-| ``asdm (string='')`` - The associated ASDM name. Must be specified
+| ``asdm (string='')`` - The associated ASDM name. Must be specified. The ASDM is not required to be on the file system; its value is simply passed to the web service.
 |                       default: ''
-|                       Example:asdm='uid___A002_X10ac6bc_X896d
+|                       Example:asdm='uid://A002/X10ac6bc/X896d'
 
 .. _tw:
 
-| ``tw (string='')`` - Optional time window to which to limit search for antenna positions. Format is of the form begin_time,end_time, where times must be specified in YYYY-MM-DDThh:mm:ss.sss format and end_time must be later than begin time. Times should be UTC.
+| ``tw (string='')`` - Optional time window in which to consider baseline measurements in the database, when calculating the antenna positions. Format is of the form begin_time,end_time, where times must be specified in YYYY-MM-DDThh:mm:ss.sss format and end_time must be later than begin time. Times should be UTC.
 |          Example: tw='2023-03-14T00:40:20,2023-03-20T17:58:20'
 
 .. _snr:
@@ -156,7 +197,7 @@ Parameter Details
 
 .. _hosts:
 
-| ``hosts (stringVec=['tbd1.alma.cl', 'tbd2.alma.cl'])`` - Priority-ranked list of hosts to query to obtain positions. Only one server that returns a list of antenna positions is required. That response will be written and no additional hosts will be queried.
+| ``hosts (stringVec=['https://asa.alma.cl/uncertainties-service/uncertainties/versions/last/measurements/casa/'])`` - Priority-ranked list of hosts to query to obtain positions. Only one server that returns a list of antenna positions is required. That response will be written and no additional hosts will be queried.
 |            Example: hosts=["server1.alma.cl", "server2.alma.cl"]
 
 
