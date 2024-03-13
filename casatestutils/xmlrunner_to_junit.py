a2be01f56847b53d0692dec5194fbfb23bfc8181
AWells <awells@nrao.edu>
2024-03-13 17:52:21 -0400

    Test Converted for use with xmlrunner module

---
+++ casatestutils/xmlrunner_to_junit.py
@@ -0,0 +1,69 @@
+import xml.etree.cElementTree as ET
+import re
+import datetime
+import socket
+import argparse
+
+def convert(filename, outfile):
+    print("Reading: ",filename )
+    with open(filename, 'r') as f:
+        data = f.read()
+    print("Converting... ")
+    results = re.findall('(?s)(?<=\<testsuite).*?(?=\<\/testsuite\>)', data)
+
+    start="""<?xml version="1.0" ?>
+    <testsuite
+    """
+
+    end="""</testsuite>"""
+    name = filename.split(".py")[0].split("/")[-1]
+
+    e = datetime.datetime.now()
+    timestamp = e.strftime('%Y-%m-%dT%H:%M:%S.%f')
+    data = ET.Element('testsuites')
+    element1 = ET.SubElement(data, 'testsuite')
+    element1.set('name', "'{}'".format(name))
+
+    tests, failures, errors, skipped = 0 , 0 , 0, 0
+    runtime = 0.0
+    for result in results:
+        s = "{}{}{}".format(start, result, end)
+        root = ET.fromstring(s)
+        if "tests" in root.attrib.keys(): tests = tests + int(root.attrib['tests'])
+        if "failures" in root.attrib.keys(): failures = failures + int(root.attrib['failures'])
+        if "errors" in root.attrib.keys(): errors = errors + int(root.attrib['errors'])
+        if "skipped" in root.attrib.keys(): skipped = skipped + int(root.attrib['skipped'])
+        runtime = runtime + float(root.attrib['time'])
+
+        for i in range(len(root)):
+            if not root[i].attrib: continue
+            #print(root[i].attrib['classname'])
+            s_elem1 = ET.SubElement(element1, 'testcase')
+            s_elem1.set('classname', "{}".format(name))
+            s_elem1.set('name', "{}.{}".format(root[i].attrib['classname'],root[i].attrib['name']))
+            s_elem1.set('time', root[i].attrib['time'])
+
+            #ss_elem1 = ET.SubElement(s_elem1, 'failure')
+            #ss_elem1.set('message', fMessage)
+            #ss_elem1.text = fMessage
+
+    element1.set('hostname', socket.gethostname())
+    element1.set('timestamp', timestamp)
+    element1.set('errors', str(errors))
+    element1.set('failures', str(failures))
+    element1.set('skipped', str(skipped))
+    element1.set('tests', str(tests))
+    element1.set('time', str(round(runtime, 3)))
+    b_xml = ET.tostring(data)
+
+    with open(outfile, "wb") as f:
+        f.write(b_xml)
+    print("File Written To: ",outfile )
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--infile", help="XMLRunner module File")
+    parser.add_argument("--outfile", help="Converted XMLRunner Output File")
+    args = parser.parse_args()
+
+    convert(args.infile, args.outfile)
