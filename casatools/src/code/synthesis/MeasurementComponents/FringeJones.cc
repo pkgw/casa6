ef07f2277a792b60114c7e914ed219ef548a740b
Neal <nschweig@nrao.edu>
2024-04-26 12:45:12 -0400

    previous removals were commented out. Now the lines are deleted

--- casatools/src/code/synthesis/MeasurementComponents/FringeJones.cc
+++ casatools/src/code/synthesis/MeasurementComponents/FringeJones.cc
@@ -2214,18 +2214,11 @@ void FringeJones::smooth(Vector<Int>& fields,
 
         times.assign(ctiter.time());
 
-        // Extract Float info
-        //if (cmplx)
-        //  fpar.assign(ctiter.casfparam("AP"));
-        //else
         fpar.assign(ctiter.fparam());
-
         fparok.assign(!ctiter.flag());
         newfparok.assign(fparok);
         IPosition fsh(fpar.shape());
 
-        // For each channel
-        //for (int ichan=0;ichan<fsh(1);++ichan) {
       blc(1)=trc(1)=fblc(1)=ftrc(1)=0;
         
           // get chan Freqs
@@ -2384,7 +2377,6 @@ void FringeJones::smooth(Vector<Int>& fields,
         // keep new ok info
         p=newp;
       } // ipar
-        //} // ichan
 
         // Put info back
         if (cmplx)
