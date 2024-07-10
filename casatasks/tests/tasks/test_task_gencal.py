0e7a0d1357c10e6094e1e0c9a7f8594ba886edea
AWells <awells@nrao.edu>
2024-07-10 11:46:18 -0400

    Update test_eop_usno and test_eop_iers to use absolute mode when using testhelper compTables

--- casatasks/tests/tasks/test_task_gencal.py
+++ casatasks/tests/tasks/test_task_gencal.py
@@ -951,7 +951,7 @@ class gencal_eoptest(unittest.TestCase):
 
         # Compare with reference file from the repository
         reference = os.path.join(datapath, 'ba123a_usno.eop')
-        self.assertTrue(th.compTables(caltab, reference, ['WEIGHT'], 0.002))
+        self.assertTrue(th.compTables(caltab, reference, ['WEIGHT'], 0.002, mode="absolute"))
 
     def test_eop_iers(self):
         """Test calibration table produced when gencal is run using an
@@ -964,7 +964,7 @@ class gencal_eoptest(unittest.TestCase):
 
         # Compare with reference file from the repository
         reference = os.path.join(datapath, 'ba123a_iers.eop')
-        self.assertTrue(th.compTables(caltab, reference, ['WEIGHT'], 0.002))
+        self.assertTrue(th.compTables(caltab, reference, ['WEIGHT'], 0.002, mode="absolute"))
 
     def test_noeop(self):
         """Test that no calibration table is produced when gencal is run on an
