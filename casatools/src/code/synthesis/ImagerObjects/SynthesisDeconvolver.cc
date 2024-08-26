1aa4f59e93ad2c19d0d874606c89d845fa037526
Srikrishna Sekhar <ssekhar@nrao.edu>
2024-08-26 16:31:36 -0600

    CAS-14201 : Intermediate commit, many cout statements
    The issue with the broken tclean default behaviour has mostly gone away.
    This commit is to preserve state as it is right now, a full explanation
    will be in the next commit once I have had a chance to confirm by
    running the test suite.

--- casatools/src/code/synthesis/ImagerObjects/SynthesisDeconvolver.cc
+++ casatools/src/code/synthesis/ImagerObjects/SynthesisDeconvolver.cc
@@ -254,6 +254,9 @@ namespace casa { //# NAMESPACE CASA - BEGIN
     //Force recalculation of robust stats to update nsigmathreshold with
     //most recent residual
 
+
+
+
     if(itsAutoMaskAlgorithm=="multithresh" && itsImages->residual()->shape()[3] >1 && itsNsigma > 0.0){
       Record retval;
       Record backupRobustStats=itsRobustStats;
@@ -310,16 +313,29 @@ namespace casa { //# NAMESPACE CASA - BEGIN
       //itsIterDone += itsLoopController.getIterDone();
 
       //      setupMask();
+      //
+      cout << "initMinorCycle .... hasMask " << itsImages->hasMask() << endl;
+      cout << "hasMask " << itsImages->hasMask() << endl;
 
       Float masksum;
+      cout << "hasMask " << itsImages->hasMask() << endl;
       if( ! itsImages->hasMask() ) // i.e. if there is no existing mask to re-use...
-	{ masksum = -1.0;}
+	  {
+          cout << "I'm in here, no mask " << endl;
+          cout << "hasMask " << itsImages->hasMask() << endl;
+          masksum = -1.0;
+      }
       else
 	{
+      cout << "I'm in here, WITH mask " << endl;
+      cout << "hasMask " << itsImages->hasMask() << endl;
 	  masksum = itsImages->getMaskSum();
 	  itsImages->mask()->unlock();
 	}
       Bool validMask = ( masksum > 0 );
+      cout << "validMask " << validMask << " masksum " << masksum << endl;
+      cout << "hasMask " << itsImages->hasMask() << endl;
+
       //    os << LogIO::NORMAL3 << "****INITMINOR Masksum stuff "<< tim.real() << LogIO::POST;
       // tim.mark();
 
@@ -335,11 +351,15 @@ namespace casa { //# NAMESPACE CASA - BEGIN
       itsLoopController.setPeakResidualNoMask( peakresnomask );
       itsLoopController.setMaxPsfSidelobe( itsImages->getPSFSidelobeLevel() );
 
+      cout << "after setPSFSidelobe hasMask " << itsImages->hasMask() << endl;
+
       //re-calculate current nsigma threhold
       //os<<"Calling calcRobustRMS ....syndeconv."<<LogIO::POST;
       Float nsigmathresh = 0.0;
       Bool useautomask = ( itsAutoMaskAlgorithm=="multithresh" ? true : false);
       Int iterdone = itsLoopController.getIterDone();
+
+      cout << "Before itsNSigma hasMask " << itsImages->hasMask() << endl;
 
       //cerr << "INIT automask " << useautomask << " alg " << itsAutoMaskAlgorithm << " sigma " << itsNsigma  << endl;
       if ( itsNsigma >0.0) {
@@ -389,6 +409,7 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 	        //cerr << this << " DOING robust " << itsRobustStats << endl;
 
        }
+      cout << "After itsNSigma hasMask " << itsImages->hasMask() << endl;
 
         /***
         Array<Double> robustrms =kitsImages->calcRobustRMS(medians, itsPBMask, itsFastNoise);
@@ -402,7 +423,7 @@ namespace casa { //# NAMESPACE CASA - BEGIN
         //Double maxrobustrms = max(robustrms);
         if(robustrms.empty())
           throw(AipsError("No valid values to deconvolve"));
-          
+
         minMax(minval, maxval, minpos, maxpos, robustrms);
 
         //Float nsigmathresh = nsigma * (Float)robustrms(IPosition(1,0));
@@ -418,9 +439,13 @@ namespace casa { //# NAMESPACE CASA - BEGIN
         os << "Current nsigma threshold (maximum along spectral channels ) ="<<nsigmathresh<< msg <<LogIO::POST;
       }
 
+      cout << "Before setPBMask hasMask " << itsImages->hasMask() << endl;
+
       itsLoopController.setNsigmaThreshold(nsigmathresh);
       itsLoopController.setPBMask(itsPBMask);
       itsLoopController.setFullSummary(itsFullSummary);
+
+      cout << "After setPBMask hasMask " << itsImages->hasMask() << endl;
 
       if ( itsAutoMaskAlgorithm=="multithresh" && !initializeChanMaskFlag ) {
         IPosition maskshp = itsImages->mask()->shape();
@@ -457,6 +482,8 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 	  itsLoopController.setMaskSum( -1.0 );
 	}
 
+      cout << "Before summaryminor hasMask " << itsImages->hasMask() << endl;
+
       // CAS-14201 : Always initialize summaryminor, even when niter=0
       for( Int chanid=0; chanid<nSubChans;chanid++) {
         for( Int polid=0; polid<nSubPols; polid++) {
@@ -464,10 +491,10 @@ namespace casa { //# NAMESPACE CASA - BEGIN
                     0 /*cycleStartIteration */,
                     0 /*startiteration */,
                     0.0 /*startmodelflux */,
-                    itsImages->getPeakResidualWithinMask() /*startpeakresidual */,
+                    itsImages->hasMask() ? itsImages->getPeakResidualWithinMask() : itsImages->getPeakResidual() /*startpeakresidual */,
                     itsImages->getPeakResidual() /*startpeakresidualnomask */,
                     itsImages->getModelFlux()/*modelflux */,
-                    itsImages->getPeakResidualWithinMask() /*peakresidual */,
+                    itsImages->hasMask() ? itsImages->getPeakResidualWithinMask() : itsImages->getPeakResidual() /*startpeakresidual */,
                     itsImages->getPeakResidual() /*peakresidualnomask */,
                     masksum,
                     0 /*rank */,
@@ -475,6 +502,9 @@ namespace casa { //# NAMESPACE CASA - BEGIN
                     itsFullSummary);
         }
       }
+
+      cout << "After summaryminor " << itsImages->hasMask() << endl;
+
       float psfsidelobelevel = itsImages->getPSFSidelobeLevel();
 
       returnRecord = itsLoopController.getCycleInitializationRecord();
@@ -537,9 +567,9 @@ namespace casa { //# NAMESPACE CASA - BEGIN
 	  iterRec.get("iterdone",iterdone);
 	}
       else throw(AipsError("SD::interactiveGui() needs valid niter, cycleniter, threshold to start up."));
-      
+
       if( ! itsImages ) itsImages = makeImageStore( itsImageName, itsNoRequireSumwt );
-      
+
       //      SDMaskHandler masker;
       String strthresh = String::toString(threshold)+"Jy";
       String strcycthresh = String::toString(cyclethreshold)+"Jy";
@@ -1089,12 +1119,12 @@ namespace casa { //# NAMESPACE CASA - BEGIN
             if( ! itsImages->hasMask() || emptyMask ) // i.e. if there is no existing mask to re-use...
             {
                 LatticeLocker lock1 (*(itsImages->mask()), FileLocker::Write);
+                cout << "itsIsInteractive " << itsIsInteractive << " emptyMask " << emptyMask << endl;
                 if( itsIsInteractive ) itsImages->mask()->set(0.0);
                 else itsImages->mask()->set(1.0);
                 os << "[" << itsImages->getName() << "] Initializing new mask to " << (itsIsInteractive?"0.0 for interactive drawing":"1.0 for the full image") << LogIO::POST;
             }
             else {
-                os <<  "hasMask " << itsImages->hasMask() << " masksum " << itsImages->getMaskSum() << " masklist " << itsMaskList[0] << " emptyMask " << emptyMask << endl;
                 os << "[" << itsImages->getName() << "] Initializing to existing mask" << LogIO::POST;
             }
         }
@@ -1161,7 +1191,7 @@ namespace casa { //# NAMESPACE CASA - BEGIN
     LogIO os( LogOrigin("SynthesisDeconvolver","checkRestoringBeam",WHERE) );
     //check for a bad restoring beam
     GaussianBeam beam;
-    
+
     if( ! itsImages ) itsImages = makeImageStore( itsImageName, itsNoRequireSumwt );
     ImageInfo psfInfo = itsImages->psf()->imageInfo();
     if (psfInfo.hasSingleBeam()) {
