3ffb7e15811f3b8e0a8fed746325730b0b8bd111
Ville Suoranta <vsuorant@nrao.edu>
2025-01-13 14:36:45 -0500

    Check for the FC variable for the Fortran compiler path and name

--- casatools/setup.py
+++ casatools/setup.py
@@ -209,7 +209,16 @@ class XmlCMakeBuildExt(build_ext):
 
         # Copy GCC libs on Macos. 
         if (sys.platform == 'darwin'):
-            gcc_dir = os.path.join(os.path.dirname(os.path.dirname(shutil.which('gfortran'))),'lib','libgcc')
+            if (os.environ.get('FC') != None):
+                fortran_compiler = os.environ.get('FC')
+            else:
+                fortran_compiler = 'gfortran'
+            
+            try:
+                gcc_dir = os.path.join(os.path.dirname(os.path.dirname(shutil.which(fortran_compiler))),'lib','libgcc')
+            except:
+                raise Exception("Couldn't get the libgcc path. Try setting the FC environment variable to your gfortran compiler.")
+            
             casac_lib_dir = extdir + "/casatools/__casac__/lib"
             copy_tree(gcc_dir, casac_lib_dir)
 
